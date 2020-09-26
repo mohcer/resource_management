bf3760856c6be50dc740f0c3e007be370c6bf2b1
Mohsin Sayyed <msayyed456@gmail.com>
2020-09-26 20:43:22 +0800

    updating to add == literal comparison instead of 'is'

--- app/test/test_config.py
+++ app/test/test_config.py
@@ -20,7 +20,7 @@ class TestDevelopmentConfig(TestCase):
         return app
 
     def test_app_is_development(self):
-        self.assertFalse(app.config["SECRET_KEY"] is "chainstack-platform")
+        self.assertFalse(app.config["SECRET_KEY"] == "chainstack-platform")
         self.assertTrue(app.config["DEBUG"] is True)
         self.assertFalse(current_app is None)
         self.assertTrue(
@@ -35,7 +35,7 @@ class TestTestingConfig(TestCase):
         return app
 
     def test_app_is_testing(self):
-        self.assertFalse(app.config["SECRET_KEY"] is "my_precious")
+        self.assertFalse(app.config["SECRET_KEY"] == "my_precious")
         self.assertTrue(app.config["DEBUG"])
         self.assertTrue(
             app.config["SQLALCHEMY_DATABASE_URI"]
@@ -49,7 +49,7 @@ class TestProductionConfig(TestCase):
         return app
 
     def test_app_is_production(self):
-        self.assertTrue(app.config["DEBUG"] is False)
+        self.assertTrue(app.config["DEBUG"] == False)
 
 
 if __name__ == "__main__":
